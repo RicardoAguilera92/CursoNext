ad75754b54305140af598caa0ff4587449a293af
RicardoAguilera92 <ricardo.aguilera92@icloud.com>
2023-07-11 17:25:10 -0600

    implementacion de componenete Chart en el dashboard

--- react-shop-admin/src/common/Chart.js
+++ react-shop-admin/src/common/Chart.js
@@ -1,7 +1,7 @@
 import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend } from 'chart.js';
 import { Bar } from 'react-chartjs-2';
 
-ChartJS.register(categoryScale, LinearScale, BarElement, Title, Tooltip, Legend);
+ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend);
 
 export const Chart = ({ chartData }) => {
   return (
